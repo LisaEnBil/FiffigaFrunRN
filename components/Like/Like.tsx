30e2490a2e00d942dc2774e050e812dc2aad2e05
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-08-20 05:35:43 +0200

    add like component

---
+++ components/Like/Like.tsx
@@ -0,0 +1,47 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { useContext } from 'react';
+import { Pressable } from 'react-native';
+import { LikesContext } from '../../contexts/LikesContext';
+import { likeStyles } from './like.styles';
+import { Item } from '@/types/global-types';
+
+interface LikeProps {
+  item: Item,
+  size: number
+}
+
+export const Like: React.FC<LikeProps> = ({ item, size }) => {
+  const context = useContext(LikesContext);
+
+  if (!context) {
+    throw new Error('Like must be used within a LikesProvider');
+  }
+
+  const { likes, dispatch } = context;
+
+  const icon = likes[item.title] ? 'favorite' : 'favorite-outline';
+
+  const addItemToList = async () => {
+    if (likes[item.title]) {
+      dispatch({
+        type: 'remove',
+        payload: {
+          itemTitle: item.title,
+        },
+      });
+    } else {
+      dispatch({
+        type: 'add',
+        payload: {
+          itemTitle: item.title,
+        },
+      });
+    }
+  };
+
+  return (
+    <Pressable style={likeStyles.likeContainer} onPress={addItemToList}>
+      <MaterialIcons name={icon} size={size} color="#e39d9c" />
+    </Pressable>
+  );
+};
---
+++ components/Like/like.styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const likeStyles = StyleSheet.create({
+  likeContainer: {
+    width: 44,
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
