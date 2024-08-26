4b6f5476588ea6c4bcd4c073ebe4d791e0c36f05
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-08-26 19:10:44 +0200

    updating tests to typescript

--- app/(tabs)/__test__/Home.test.ts
+++ app/(tabs)/__test__/Home.test.ts
@@ -1,37 +1,66 @@
-import { getRandomInt } from '@/helpers/helpers';
+// import { getRandomInt } from '@/helpers/helpers';
 
-describe('getRandomInt function', () => {
-  test('returns a random integer within the specified range', () => {
-    const min = 1;
-    const max = 10;
-    const randomInt = getRandomInt(min, max);
 
-    expect(randomInt).toBeGreaterThanOrEqual(min);
-    expect(randomInt).toBeLessThanOrEqual(max);
-  });
 
-  test('returns an integer when min and max are the same', () => {
-    const min = 5;
-    const max = 5;
-    const randomInt = getRandomInt(min, max);
 
-    expect(randomInt).toEqual(min);
-  });
+// test('returns a random integer within the specified range', () => {
+//   const min = 1;
+//   const max = 10;
+//   const randomInt = getRandomInt(min, max);
 
-  // test('returns NaN if min or max are not numbers', () => {
-  //   const min = 'abc';
-  //   const max = 10;
-  //   const randomInt = getRandomInt(min, max);
+//   expect(randomInt).toBeGreaterThanOrEqual(min);
+//   expect(randomInt).toBeLessThanOrEqual(max);
+// });
 
-  //   expect(randomInt).toBeNaN();
-  // });
+// test('returns an integer when min and max are the same', () => {
+//   const min = 5;
+//   const max = 5;
+//   const randomInt = getRandomInt(min, max);
 
-  test('throws error if max is less than min', () => {
-    const min = 10;
-    const max = 5;
+//   expect(randomInt).toEqual(min);
+// });
 
-    expect(() => {
-      getRandomInt(min, max);
-    }).toThrow('max is less than min');
-  });
-});
+// test('throws error if max is less than min', () => {
+//   const min = 10;
+//   const max = 5;
+
+//   expect(() => {
+//     getRandomInt(min, max);
+//   }).toThrow('max is less than min');
+// });
+
+// describe('getRandomInt function', () => {
+//   test('returns a random integer within the specified range', () => {
+//     const min = 1;
+//     const max = 10;
+//     const randomInt = getRandomInt(min, max);
+
+//     expect(randomInt).toBeGreaterThanOrEqual(min);
+//     expect(randomInt).toBeLessThanOrEqual(max);
+//   });
+
+//   test('returns an integer when min and max are the same', () => {
+//     const min = 5;
+//     const max = 5;
+//     const randomInt = getRandomInt(min, max);
+
+//     expect(randomInt).toEqual(min);
+//   });
+
+//   // test('returns NaN if min or max are not numbers', () => {
+//   //   const min = 'abc';
+//   //   const max = 10;
+//   //   const randomInt = getRandomInt(min, max);
+
+//   //   expect(randomInt).toBeNaN();
+//   // });
+
+//   test('throws error if max is less than min', () => {
+//     const min = 10;
+//     const max = 5;
+
+//     expect(() => {
+//       getRandomInt(min, max);
+//     }).toThrow('max is less than min');
+//   });
+// });
