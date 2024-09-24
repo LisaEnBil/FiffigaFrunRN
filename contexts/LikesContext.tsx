0fe91939b26c0fa7c022f4a731806462fcc2a514
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2024-09-24 05:52:16 +0200

    clean up context

--- contexts/LikesContext.tsx
+++ contexts/LikesContext.tsx
@@ -1,12 +1,4 @@
-import { reducer } from '@/components/Providers/LikesProvider';
-import { LikedItem } from '@/types/global-types';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import React, { createContext, useEffect, useReducer, ReactNode } from 'react';
+import React, { createContext } from 'react';
 
-interface LikesState {
-    likes: Record<string, LikedItem>;
-    dispatch: React.Dispatch<any>;
-}
-
-export const LikesContext = createContext<LikesState | null>({ likes: {}, dispatch: () => null });
+export const LikesContext = createContext<any>({});
 
